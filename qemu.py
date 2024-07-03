eadccbd955073ad117d1c79481a0b4b18eef5dc8
Aiden Pearce <104835550+Artemon4ik8091@users.noreply.github.com>
2024-07-03 13:45:38 +0300

    Update

--- qemu.py
+++ qemu.py
@@ -4,15 +4,15 @@ print("| Qemu Starter Script |")
 print("-----------------------")
 start = True
 while(start == True):
-    mode = input("Select mode (Create virtual disk or Start virtual machine) or enter exit to close script: ")
-    if (mode == "Create virtual disk"):
+    mode = input("Select mode (1. Create virtual disk or 2. Start virtual machine) or enter exit to close script: ")
+    if (mode == "Create virtual disk" or "1"):
         size = input("Enter size to disk (Gb): ")
         name = input("Enter name to disk: ")
         os.system("qemu-img create -f qcow2 " + name + ".qcow2 " + size + "G")
         print("Disk " + name + " created!")
-    if (mode == "Start virtual machine"):
-        config = input("Select configuration (macOS or other): ")
-        if (config == "macOS"):
+    if (mode == "Start virtual machine" or "2"):
+        config = input("Select configuration (1. macOS or 2. other): ")
+        if (config == "macOS" or "1"):
             drive_c = input("Enter file patch to C Drive: ")
             drive_d_enabled = True
             drive_d = input("Enter file patch to D drive (or press enter to skip): ")
@@ -24,8 +24,14 @@ while(start == True):
                 os.system("qemu-system-ppc -L pc-bios -boot c -M mac99 -m " + memory + " -drive file=" + drive_d + ",format=raw,media=cdrom -drive file=" + drive_c + ",format=qcow2,media=disk " + param)
             if(drive_d_enabled == False):
                 os.system("qemu-system-ppc -L pc-bios -boot c -M mac99 -m " + memory + " -drive file=" + drive_c + ",format=qcow2,media=disk " + param)
-        if (config == "other"):
-            architecture_select = input("Select qemu package (qemu-system-i386 (x86 systems), qemu-system-ppc (PowerPC systems), qemu-system-arm (Arm systems)): ")
+        if (config == "other" or "2"):
+            architecture_select = input("Select qemu package (1. qemu-system-i386 (x86 systems), 2. qemu-system-ppc (PowerPC systems), 3. qemu-system-arm (Arm systems)): ")
+            if (architecture_select == "1"):
+                architecture_select = "qemu-system-i386"
+            if (architecture_select == "2"):
+                architecture_select = "qemu-system-ppc"
+            if (architecture_select == "1"):
+                architecture_select = "qemu-system-arm"
             drive_c = input("Enter file patch to C Drive: ")
             drive_d_enabled = True
             drive_d = input("Enter file patch to D drive (or press enter to skip): ")
